845502efaf9818a7c7e2cf12124184c5b56d6027
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2026-03-14 00:27:44 +0530

    install: tsj.s

--- rescue-mesh/dev-dist/sw.js
+++ rescue-mesh/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.lp3sf22nf2c"
+    "revision": "0.kg9cqskjq64"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
