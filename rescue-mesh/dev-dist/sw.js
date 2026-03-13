680aeea176cf70f45916d9b4c3424fefa3db7a3c
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2026-03-13 20:52:37 +0530

    update: sw

--- rescue-mesh/dev-dist/sw.js
+++ rescue-mesh/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.e6ncpinq698"
+    "revision": "0.lp3sf22nf2c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
